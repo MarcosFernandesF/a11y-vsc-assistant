0391b1d037e87bba09c1a3aff00118ee77bd79bc
MarcosFernandesF <marcosrff15@gmail.com>
2026-03-21 22:40:36 -0300

    Adicionando interface RuleError

---
+++ src/rules/types.ts
@@ -0,0 +1,9 @@
+/**
+ * Interface que representa o contrato de um erro de acessibilidade.
+ * Utilizada para padronizar o retorno de todas as funções de validação.
+ */
+export interface RuleError {
+  tag: string;
+  index: number;
+  message: string;
+}
