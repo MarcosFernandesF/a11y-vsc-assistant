d2dd6ef707d4a89efe9d0bfdf0b5ed1dd64cda36
MarcosFernandesF <marcosrff15@gmail.com>
2026-03-22 10:07:20 -0300

    Criando motor de regras para ordem dos headers

---
+++ src/rules/headersRules.ts
@@ -0,0 +1,61 @@
+import { RuleError } from "./types";
+
+/**
+ * Valida se os cabeçalhos estão em um ordem lógica (h1 a h6).
+ * @param text O conteúdo HTML a ser análisado.
+ */
+export function validateHeadersOrder(text: string): RuleError[] {
+  const errors: RuleError[] = [];
+
+  // Identifica se possui headers -> <h seguido de um número de 1 a 6.
+  const headerRegex = /<h([1-6])\b[^>]*>/gi;
+
+  let match;
+  const allHeaders:
+    {
+      text: string;
+      headerLevel: number;
+      index: number
+    }[] = [];
+
+  while ((match = headerRegex.exec(text)) !== null) {
+    const headerTag = match[0];
+    const headerLevel = Number(match[1]);
+    allHeaders.push({
+      text: headerTag,
+      headerLevel,
+      index: match.index,
+    });
+  }
+
+  let previousLevel = 0;
+  for (const header of allHeaders) {
+    if (previousLevel !== 0 && header.headerLevel > previousLevel + 1) {
+      errors.push({
+        tag: header.text,
+        index: header.index,
+        message: `Erro de Acessibilidade: Pulo de hierarquia detectado (${header.text}).`,
+      });
+    }
+
+    if (previousLevel === 1 && header.headerLevel === 1) {
+      errors.push({
+        tag: header.text,
+        index: header.index,
+        message: `Erro de Acessibilidade: Não pode haver múltiplos h1 na página (${header.text}).`,
+      });
+    }
+
+    if (previousLevel !== 0 && header.headerLevel < previousLevel) {
+      errors.push({
+        tag: header.text,
+        index: header.index,
+        message: `Erro de Acessibilidade: Hierarquia de headers deve ser crescente (${header.text}).`,
+      });
+    }
+
+    previousLevel = header.headerLevel;
+  }
+
+  return errors;
+}
