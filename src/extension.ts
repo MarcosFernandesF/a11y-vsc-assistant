9287ab4562f04bc77b083e494663e4dc5f68e388
MarcosFernandesF <marcosrff15@gmail.com>
2026-03-21 22:29:52 -0300

    Importando regra no extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode';
+import { validateImagesWithoutAlt } from './rules/imageRules';
 
 let timeout: NodeJS.Timeout | undefined = undefined;
 
@@ -8,7 +9,7 @@ export function activate(context: vscode.ExtensionContext) {
 	if (vscode.window.activeTextEditor) {
 		const doc = vscode.window.activeTextEditor.document;
 		if (isFileExtensionValid(doc)) {
-			imagesWithNoAltTextValidation(doc);
+			validateImagesWithoutAlt(doc.getText());
 		}
 	}
 
@@ -21,7 +22,7 @@ export function activate(context: vscode.ExtensionContext) {
 			}
 
 			timeout = setTimeout(() => {
-				imagesWithNoAltTextValidation(document);
+				validateImagesWithoutAlt(document.getText());
 			}, 500);
 		}
 	});
@@ -38,30 +39,3 @@ export function deactivate() {
 function isFileExtensionValid(document: vscode.TextDocument) {
 	return document.languageId === 'html' || document.languageId === 'css';
 }
-
-function imagesWithNoAltTextValidation(document: vscode.TextDocument) {
-	const text = document.getText();
-
-	// Regex ampla para capturar TODAS as tags <img>, independente do conteúdo
-	const regexTagImg = /<img[^>]*>/gi;
-
-	// Regex específica para validar se o 'alt' existe e TEM CONTEÚDO (independente da ordem dos atributos)
-	const regexValidAlt = /<img\s+(?=[^>]*?\balt=["'](?!\s*["'])[^"']+?["'])[^>]*?>/i;
-
-	let match;
-	while ((match = regexTagImg.exec(text)) !== null) {
-		const entireTag = match[0];
-
-		const hasValidAlt = regexValidAlt.test(entireTag);
-
-		if (!hasValidAlt) {
-			const inicioScan = document.positionAt(match.index);
-
-			console.log(`Erro de Acessibilidade na Linha ${inicioScan.line + 1}: Tag <img> sem atributo 'alt' ou com 'alt' vazio.`);
-		}
-		else {
-			console.log("Alt sendo utilizado!");
-		}
-	}
-}
-
