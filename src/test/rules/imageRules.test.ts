df583edc91622249a24a7386e74aac6d9de9f14d
MarcosFernandesF <marcosrff15@gmail.com>
2026-03-22 10:06:53 -0300

    Adicionando caso de múltiplos erros

--- src/test/rules/imageRules.test.ts
+++ src/test/rules/imageRules.test.ts
@@ -9,7 +9,12 @@ function runImageTests() {
         { name: "Atributo alt ausente", html: '<img src="teste.png">', expected: 1 },
         { name: "Atributo alt vazio", html: '<img src="teste.png" alt="">', expected: 1 },
         { name: "Alt valido", html: '<img src="teste.png" alt="Descricao">', expected: 0 },
-        { name: "Alt preenchido em ordem diferente", html: '<img alt="Descrição valida" src="teste.png"', expected: 0 }
+        { name: "Alt preenchido em ordem diferente", html: '<img alt="Descrição valida" src="teste.png"', expected: 0 },
+        { name: "Duas imagens sem alt", html: '<img src="img1.png"><img src="img2.png">', expected: 2 },
+        { name: "Duas imagens com alt vazio", html: '<img src="img1.png" alt=""><img src="img2.png" alt="">', expected: 2 },
+        { name: "Multiplas imagens sem alt", html: '<img src="img1.png"><img src="img2.png" alt=""><img src="img3.png">', expected: 3 },
+        { name: "Mix de imagens validas e invalidas", html: '<img src="img1.png" alt="Ok"><img src="img2.png"><img src="img3.png" alt="Ok"><img src="img4.png" alt="">', expected: 2 },
+        { name: "Sem imagens", html: '<p>Apenas paragrafo</p>', expected: 0 }
     ];
 
     let passedCount = 0;
