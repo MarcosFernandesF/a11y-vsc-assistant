69ea2c781c9f5e042b093d8198f5a78f09832525
MarcosFernandesF <marcosrff15@gmail.com>
2026-03-21 21:59:04 -0300

    Criando regra para imagem sem alt

--- out/extension.js
+++ out/extension.js
@@ -39,14 +39,20 @@ const vscode = __importStar(require("vscode"));
 let timeout = undefined;
 function activate(context) {
     console.log('A11y Assistant Funcionando.');
+    if (vscode.window.activeTextEditor) {
+        const doc = vscode.window.activeTextEditor.document;
+        if (isFileExtensionValid(doc)) {
+            imagesWithNoAltTextValidation(doc);
+        }
+    }
     const documentChangeEvent = vscode.workspace.onDidChangeTextDocument(event => {
         const document = event.document;
-        if (document.languageId === 'html' || document.languageId === 'css') {
+        if (isFileExtensionValid(document)) {
             if (timeout) {
                 clearTimeout(timeout);
             }
             timeout = setTimeout(() => {
-                console.log("Evento escutado: ", event);
+                imagesWithNoAltTextValidation(document);
             }, 500);
         }
     });
@@ -55,6 +61,28 @@ function activate(context) {
 function deactivate() {
     if (timeout) {
         clearTimeout(timeout);
+    }
+}
+function isFileExtensionValid(document) {
+    return document.languageId === 'html' || document.languageId === 'css';
+}
+function imagesWithNoAltTextValidation(document) {
+    const text = document.getText();
+    // Regex ampla para capturar TODAS as tags <img>, independente do conteúdo
+    const regexTagImg = /<img[^>]*>/gi;
+    // Regex específica para validar se o 'alt' existe e TEM CONTEÚDO (independente da ordem dos atributos)
+    const regexValidAlt = /<img\s+(?=[^>]*?\balt=["'](?!\s*["'])[^"']+?["'])[^>]*?>/i;
+    let match;
+    while ((match = regexTagImg.exec(text)) !== null) {
+        const entireTag = match[0];
+        const hasValidAlt = regexValidAlt.test(entireTag);
+        if (!hasValidAlt) {
+            const inicioScan = document.positionAt(match.index);
+            console.log(`Erro de Acessibilidade na Linha ${inicioScan.line + 1}: Tag <img> sem atributo 'alt' ou com 'alt' vazio.`);
+        }
+        else {
+            console.log("Alt sendo utilizado!");
+        }
     }
 }
 //# sourceMappingURL=extension.js.map
